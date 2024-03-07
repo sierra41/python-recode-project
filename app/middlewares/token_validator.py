8d9f5eba724ea4a221bb3598f220d9ead3a00459
sierra41 <actin45@gmail.com>
2024-03-07 09:15:33 +0900

    fix: convert

--- app/middlewares/token_validator.py
+++ app/middlewares/token_validator.py
@@ -3,6 +3,7 @@ import typing
 import re
 import jwt
 
+
 from fastapi.params import Header
 from jwt import PyJWTError
 from pydantic import BaseModel
@@ -44,18 +45,20 @@ class AccessControl:
         headers = Headers(scope=scope)
 
         #request.stats.temptemptmep = "무슨 값이든 적을 수 있음" : 미들웨어에서 체크한 값을 엔드포인트에서 사용가능하도록 값을 입력하는 용도
-        request.state.req_time = D.datetime() # 요청시간
+        request.state.req_time = D.datetime() # 요청시간w
 
         request.state.start = time.time() # 해당 엔드포인트가 실행하는 데 걸린 시간을 확인하기 위하여
-        request.state.inspect = None #500에러 - 어떤 파일 몇번째줄에서 에러가 발생한건지 로깅용 # sentre
-        request.state.user = None #token decording 내용. 가능하면 db조회를 최소화
+        request.state.inspect = None # 500에러 - 어떤 파일 몇번째줄에서 에러가 발생한건지 로깅용 # sentre
+        request.state.user = None # token decording 내용. 가능하면 db조회를 최소화
         request.state.is_admin_access = None
-        ip_from = request.headers["x-forwarded-for"] if "x-forwarded-for" in request.headers.keys() else None
-        # x-forwarded-for : aws ip
+        # 값들을 = None 으로 설정해둬야 나중에 호출시 에러가 발생하지 않음.
 
-        if await self.url_pattern_check(request.url.path,
-                                        self.except_path_regex) or request.url.path in self.except_path_list:
+        ip_from = request.headers["x-forwarded-for"] if "x-forwarded-for" in request.headers.keys() else None
+        # x-forwarded-for : aws 아이피 추출용
+
+        if await self.url_pattern_check(request.url.path, self.except_path_regex) or request.url.path in self.except_path_list:
             return await self.app(scope, receive, send)
+            # except_path_regex, except_path_list check  - 더 이상 검증하지 않고 되돌아감
 
         if request.url.path.startswith("/api"):
             # api 인경우 헤더로 토큰 검사
@@ -67,9 +70,9 @@ class AccessControl:
                     response = JSONResponse(status_code=401, content=dict(msg="AUTHORIZATION_REQUIRED"))
                     return await response(scope, receive, send)
         else:
-            # 템플릿 렌더링인 경우 쿠키에서 토큰 검사
+            # 템플릿 렌더링인 경우 쿠키에서 토큰 검사 - 프론트가 같이 개발되는 경우
             print(request.cookies)
-            # request.cookies["Authorization"] = "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6MTQsImVtYWlsIjoia29hbGFAZGluZ3JyLmNvbSIsIm5hbWUiOm51bGwsInBob25lX251bWJlciI6bnVsbCwicHJvZmlsZV9pbWciOm51bGwsInNuc190eXBlIjpudWxsfQ.4vgrFvxgH8odoXMvV70BBqyqXOFa2NDQtzYkGywhV48"
+            request.cookies["Authorization"] = "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6MTQsImVtYWlsIjoia29hbGFAZGluZ3JyLmNvbSIsIm5hbWUiOm51bGwsInBob25lX251bWJlciI6bnVsbCwicHJvZmlsZV9pbWciOm51bGwsInNuc190eXBlIjpudWxsfQ.4vgrFvxgH8odoXMvV70BBqyqXOFa2NDQtzYkGywhV48"
 
             if "Authorization" not in request.cookies.keys():
                 response = JSONResponse(status_code=401, content=dict(msg="AUTHORIZATION_REQUIRED"))
@@ -78,11 +81,14 @@ class AccessControl:
             request.state.user = await self.token_decode(access_token=request.cookies.get("Authorization"))
 
         request.state.req_time = D.datetime()
+
         print(D.datetime()) # 2024-03-06 06:22:51.908516
         print(D.date()) # 2024-03-06
         print(D.date_num()) # 20240306
-
         print(request.cookies)
+        request.cookies[
+            "Authorization"] = "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6MTQsImVtYWlsIjoia29hbGFAZGluZ3JyLmNvbSIsIm5hbWUiOm51bGwsInBob25lX251bWJlciI6bnVsbCwicHJvZmlsZV9pbWciOm51bGwsInNuc190eXBlIjpudWxsfQ.4vgrFvxgH8odoXMvV70BBqyqXOFa2NDQtzYkGywhV48"
+
         print(headers)
         #Headers({'host': '0.0.0.0:8080', 'connection': 'keep-alive', 'cache-control': 'max-age=0', 'upgrade-insecure-requests': '1', 'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36', 'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7', 'accept-encoding': 'gzip, deflate', 'accept-language': 'ko-KR,ko;q=0.9,en-US;q=0.8,en;q=0.7'})
         res = await self.app(scope, receive, send)
@@ -104,6 +110,8 @@ class AccessControl:
         try:
             access_token = access_token.replace("Bearer ", "")
             payload = jwt.decode(access_token, key=consts.JWT_SECRET, algorithms=[consts.JWT_ALGORITHM])
+            # token 생성시 활용한  key=consts.JWT_SECRET, algorithms=[consts.JWT_ALGORITHM]으로 디코딩
+
         except PyJWTError as e:
             print(e)
             # Raise Error
